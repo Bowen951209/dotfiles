8c1945dfaa3d1725c0a5c4aa0ee507f83bc1dc3b
bowen951209 <bowen951209@gmail.com>
2025-11-05 07:57:09 +0800

    Add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,512 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{
+		"nvim-telescope/telescope.nvim",
+		-- tag = "0.1.8",
+		version = false,
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"numToStr/Comment.nvim",
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"stevearc/aerial.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"mason-org/mason.nvim",
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim" },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^6", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"hrsh7th/cmp-path",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load({
+				exclude = { "rust" },
+			})
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "copilot" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "path" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+	{ "stevearc/conform.nvim" },
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				"copilotlsp-nvim/copilot-lsp",
+				config = function()
+					vim.g.copilot_nes_debounce = 500
+				end,
+			},
+		},
+		config = function()
+			require("copilot").setup({})
+		end,
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+			nes = { enabled = true },
+			filetypes = {
+				rust = true,
+				lua = true,
+				["*"] = false,
+			},
+		},
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		keys = {
+			{
+				"<leader>b",
+				"<cmd> DapToggleBreakpoint<cr>",
+				desc = "Toggle Breakpoint",
+			},
+		},
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "codelldb" },
+				automatic_installation = true,
+			})
+		end,
+	},
+	{ "j-hui/fidget.nvim", opts = {} },
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim", -- Diff integration
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{ "lewis6991/gitsigns.nvim" },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"chrisgrieser/nvim-lsp-endhints",
+		event = "LspAttach",
+		opts = {}, -- required, even if empty
+	},
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find word under cursor" })
+vim.keymap.set("n", "<leader>fc", builtin.command_history, { desc = "Command history" })
+vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix list" })
+
+require("nvim-treesitter").setup({
+	-- Directory to install parsers and queries to
+	install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+require("nvim-treesitter").install({ "rust", "javascript", "lua" })
+
+-- highlighting
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "rust", "javascript", "lua" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+
+-- folding and indenting
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+vim.opt.showmode = false
+require("lualine").setup({
+	options = { theme = "codedark" },
+})
+
+vim.opt.termguicolors = true
+require("bufferline").setup({})
+
+require("Comment").setup()
+
+require("aerial").setup({
+	-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+	on_attach = function(bufnr)
+		-- Jump forwards/backwards with '{' and '}'
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
+})
+
+-- keymap to toggle aerial
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls" },
+	automatic_enable = {
+		exclude = {
+			"rust_analyzer",
+		},
+	},
+})
+
+-- This prevent `undefined global vim` warnings.
+-- Source - https://stackoverflow.com/questions/79647620/undefined-global-vim
+-- Posted by Jo Totland
+-- Retrieved 2025/11/5, License - CC-BY-SA 4.0
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rustfmt" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
+
+-- format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+})
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
+
+-- DAP Configuration for Rust (codelldb)
+local dap = require("dap")
+
+-- Configure CodeLLDB adapter
+dap.adapters.codelldb = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = vim.fn.has("win32") == 1 and vim.fn.stdpath("data") .. "/mason/bin/codelldb.cmd"
+			or vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+		args = { "--port", "${port}" },
+	},
+}
+
+-- Rust Debugging Configuration
+dap.configurations.rust = {
+	{
+		name = "Launch Rust program",
+		type = "codelldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+	},
+}
+
+require("dapui").setup()
+
+local dapui = require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+require("neogit").setup({
+	integrations = {
+		diffview = true,
+	},
+})
+
+vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<CR>")
+
+-- Copy to system clipboard (visual mode)
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+yg_', { desc = "Yank line to system clipboard" })
+
+-- Paste from system clipboard
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set("n", "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+
+-- Inlay hint
+vim.keymap.set("n", "<leader>ih", function()
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "Toggle Inlay Hints" })
+
+require("kanagawa").load("dragon")
+
+require("lsp-endhints").setup({
+	icons = {
+		type = "󰜁 ",
+		parameter = "󰏪 ",
+		offspec = " ", -- hint kind not defined in official LSP spec
+		unknown = " ", -- hint kind is nil
+	},
+	label = {
+		truncateAtChars = 20,
+		padding = 1,
+		marginLeft = 0,
+		sameKindSeparator = ", ",
+	},
+	extmark = {
+		priority = 50,
+	},
+	autoEnableHints = true,
+})
+
+vim.keymap.set(
+	"n",
+	"<Leader>th",
+	":split | terminal<CR>",
+	{ noremap = true, silent = true, desc = "Open horizontal terminal" }
+)
+
+vim.keymap.set(
+	"n",
+	"<Leader>tv",
+	":vsplit | terminal<CR>",
+	{ noremap = true, silent = true, desc = "Open vertical terminl" }
+)
+
+vim.keymap.set(
+	"t",
+	"<Esc><Esc>",
+	"<C-\\><C-n>",
+	{ noremap = true, silent = true, desc = "Switch to normal mode from terminal mode" }
+)
+
+local function close_terminal()
+	local buftype = vim.bo.buftype
+	if buftype == "terminal" then
+		vim.cmd("stopinsert") -- exit terminal mode
+		vim.cmd("bd!") -- delete buffer
+	end
+end
+vim.keymap.set("t", "<C-c><C-c>", close_terminal, { noremap = true, silent = true, desc = "Close terminal" })
+vim.keymap.set("n", "<C-c><C-c>", close_terminal, { noremap = true, silent = true, desc = "Close terminal" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true, desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true, desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true, desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true, desc = "Move to right window" })
+
+-- Run Rust "runnables"
+vim.keymap.set("n", "<leader>r", ":RustLsp! runnables<CR>", { noremap = true, silent = true, desc = "Rust runnables" })
+
+-- Debug Rust "debuggables"
+vim.keymap.set(
+	"n",
+	"<leader>d",
+	":RustLsp! debuggables<CR>",
+	{ noremap = true, silent = true, desc = "Rust debuggables" }
+)
