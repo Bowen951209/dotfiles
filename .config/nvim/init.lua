a716028fa77c714db96f77b14cb536965806e19f
bowen951209 <bowen951209@gmail.com>
2025-11-05 20:26:53 +0800

    Add vimtex.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -258,6 +258,15 @@ local plugins = {
 		event = "LspAttach",
 		opts = {}, -- required, even if empty
 	},
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_compiler_method = "latexmk"
+		end,
+	},
 }
 local opts = {}
 
