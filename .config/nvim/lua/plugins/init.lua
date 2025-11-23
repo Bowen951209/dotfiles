3cd9d091e1da2c9b60c7f6ce3ff22765be86392e
bowen951209 <bowen951209@gmail.com>
2025-11-23 08:14:09 +0800

    Add `octo.nvim`.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -276,8 +276,49 @@ local plugins = {
 		end,
 	},
 	{ "archibate/lualine-time" },
+	{
+		"pwntester/octo.nvim",
+		cmd = "Octo",
+		opts = {
+			picker = "telescope",
+			enable_builtin = true,
+		},
+		keys = {
+			{
+				"<leader>oi",
+				"<CMD>Octo issue list<CR>",
+				desc = "List GitHub Issues",
+			},
+			{
+				"<leader>op",
+				"<CMD>Octo pr list<CR>",
+				desc = "List GitHub PullRequests",
+			},
+			{
+				"<leader>od",
+				"<CMD>Octo discussion list<CR>",
+				desc = "List GitHub Discussions",
+			},
+			{
+				"<leader>on",
+				"<CMD>Octo notification list<CR>",
+				desc = "List GitHub Notifications",
+			},
+			{
+				"<leader>os",
+				function()
+					require("octo.utils").create_base_search_command({ include_current_repo = true })
+				end,
+				desc = "Search GitHub",
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
 local opts = {}
 
 return plugins, opts
-
