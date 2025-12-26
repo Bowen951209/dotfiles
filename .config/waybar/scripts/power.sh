5c2ddade4c38adfa0d9177ff5384e7a261e634bc
bowen951209 <bowen951209@gmail.com>
2025-12-26 15:05:33 +0800

    Add hibernate option in wofi power menu

--- .config/waybar/scripts/power.sh
+++ .config/waybar/scripts/power.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Present options with wofi dmenu style
-chosen=$(printf " Lock\n Logout\n Reboot\n Poweroff\n󰒲 Suspend\n󰒱 Skip Suspend" \
-    | wofi --dmenu --prompt "Power Menu" --hide-scroll --width 200 --height 150 --insensitive)
+chosen=$(printf " Lock\n Logout\n Reboot\n Poweroff\n󰒲 Suspend\n󰤄 Hibernate\n󰒱 Skip Suspend" \
+    | wofi --dmenu --prompt "Power Menu" --hide-scroll --width 200 --height 200 --insensitive)
 
 case "$chosen" in
     " Lock")
@@ -19,6 +19,9 @@ case "$chosen" in
         ;;
     "󰒲 Suspend")
         systemctl suspend
+        ;;
+    "󰤄 Hibernate")
+        systemctl hibernate
         ;;
     "󰒱 Skip Suspend")
         ~/.local/bin/touch_skip_suspend.sh
