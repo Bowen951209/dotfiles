d3f4e4a7933f6f821a126a0836d65ed2f7b402fe
bowen951209 <bowen951209@gmail.com>
2026-01-10 20:56:19 +0800

    Remove lualine-time.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,21 +1,9 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "archibate/lualine-time" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
 			options = { theme = "codedark" },
-			sections = {
-				lualine_x = {
-					"cdate",
-					{
-						"ctime",
-						format = "%H:%M",
-						fmt = function(str)
-							return str .. "  "
-						end,
-					},
-				},
-			},
 		})
 	end,
 }
