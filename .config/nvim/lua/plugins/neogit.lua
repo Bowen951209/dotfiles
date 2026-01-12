6e82cfa639b8ba0bc0756cb77e9f3261bf7582bb
bowen951209 <bowen951209@gmail.com>
2026-01-12 11:23:05 +0800

    Remove diffview.nvim

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,13 +2,17 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
 		require("neogit").setup({
 			integrations = {
-				diffview = true,
+				diffview = false,
+			},
+			mappings = {
+				popup = {
+					["d"] = false,
+				},
 			},
 		})
 	end,
