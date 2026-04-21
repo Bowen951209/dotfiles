a973ecbdac4cbce983e819da6081affd63f89b74
bowen951209 <bowen951209@gmail.com>
2026-04-21 20:18:54 +0800

    Move undefined global vim warnings fix to lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -7,3 +7,17 @@ vim.lsp.config("tsserver", {
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
+
+-- This prevents `undefined global vim` warnings.
+-- Source - https://stackoverflow.com/questions/79647620/undefined-global-vim
+-- Posted by Jo Totland
+-- Retrieved 2025/11/5, License - CC-BY-SA 4.0
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
