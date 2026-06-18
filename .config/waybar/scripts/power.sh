1fedcc387d7671f9cbeb92b77bcc39f49605d0d2
Bowen Ho <bowen951209@gmail.com>
2026-06-18 15:33:23 +0800

    Remove auto lock and suspend

--- .config/waybar/scripts/power.sh
+++ .config/waybar/scripts/power.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Present options with wofi dmenu style
-chosen=$(printf " Lock\n Logout\n Reboot\n Poweroff\n󰒲 Suspend\n󰤄 Hibernate\nSkip Suspend\nSkip Lock\nClear Skip Suspend\nClear Skip Lock" \
-    | wofi --dmenu --prompt "Power Menu" --hide-scroll --width 200 --height 300 --insensitive)
+chosen=$(printf " Lock\n Logout\n Reboot\n Poweroff\n󰒲 Suspend\n󰤄 Hibernate\n" \
+    | wofi --dmenu --prompt "Power Menu" --hide-scroll --width 200 --height 200 --insensitive)
 
 case "$chosen" in
     " Lock")
@@ -22,17 +22,5 @@ case "$chosen" in
         ;;
     "󰤄 Hibernate")
         systemctl hibernate
-        ;;
-    "Skip Suspend")
-        touch /tmp/skip_suspend
-        ;;
-    "Skip Lock")
-        touch /tmp/skip_lock
-        ;;
-    "Clear Skip Suspend")
-        rm -f /tmp/skip_suspend
-        ;;
-    "Clear Skip Lock")
-        rm -f /tmp/skip_lock
         ;;
 esac
