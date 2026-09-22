e12e35be2679c4d60f9d4c51235246a71256fa62
Bowen Ho <bowen951209@gmail.com>
2026-09-22 20:27:51 +0800

    script(pinentry): use pinentry-tty and pinentry-gnome3

--- .local/bin/smart-pinentry.sh
+++ .local/bin/smart-pinentry.sh
@@ -2,8 +2,8 @@
 # source from here: <https://askubuntu.com/a/858947>
 set -eu
 
-PINENTRY_TERMINAL='/usr/bin/pinentry-curses'
-PINENTRY_GUI='/usr/bin/pinentry-qt'
+PINENTRY_TERMINAL='/usr/bin/pinentry-tty'
+PINENTRY_GUI='/usr/bin/pinentry-gnome3'
 
 if [ -n "${DISPLAY-}" ] && [ -z "${TERM-}" ]; then
     exec "$PINENTRY_GUI" "$@"
